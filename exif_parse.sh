709006a7e822acce2a58dc3d65eadd4058ccf15d
Robin Lockwood <rlockwood@wl-144-47.wireless.umt.edu>
2019-01-28 10:28:41 -0700

    bash script now takes a single file as an argument

--- exif_parse.sh
+++ exif_parse.sh
@@ -7,8 +7,7 @@
 # Get the directory to look for images from terminal
 directory="$1"
 # get all images
-directory="$directory"'/*.jpg'
-
+directory="$directory"
 # define some regex patterns
 regex='exif:FocalLength: ([0-9]+)/([0-9]+)'
 width_reg='exif:ExifImageWidth: ([0-9]+)'
@@ -21,17 +20,17 @@ exifoutput=$(identify -verbose "$f" | grep exif:);
 # get focal length
 eo=$(echo "$exifoutput" | grep exif:FocalLength:);
 [[ $eo =~ $regex ]]
-echo "$f"
+# echo "$f"
 mmLength=$(echo "scale=3 ; ${BASH_REMATCH[1]} / ${BASH_REMATCH[2]}" | bc)
-echo "$mmLength"
+# echo "$mmLength"
 
 # get image width
 ew=$(echo "$exifoutput" | grep exif:ExifImageWidth:);
 [[ $ew =~ $width_reg ]]
 width=$(echo "${BASH_REMATCH[1]}")
-echo "$width"
+# echo "$width"
 pix=$(echo "scale=5 ; $mmLength / 36" | bc)
-echo "$pix"
+# echo "$pix"
 pixWidth=$(echo "scale=5 ; $pix * $width" | bc)
 echo "$pixWidth"
 
